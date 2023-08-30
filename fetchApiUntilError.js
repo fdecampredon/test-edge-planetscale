88a613bf6d0d5f7ef6df5cfab0d09e5a22a7d947
François de Campredon <francois.de.campredon@gmail.com>
2023-08-30 18:22:41 +0200

    add script causing failure

---
+++ fetchApiUntilError.js
@@ -0,0 +1,19 @@
+const nbFetch = 20
+
+const main = async () => {
+  while (true) {
+    fetch(
+      `https://test-edge-planetscale.vercel.app/api/fetch-planetscale?nbFetch=${nbFetch}`
+    ).then(async (response) => {
+      if (response.status !== 200) {
+        console.log(
+          `Error fetching data status : ${response.status} ${await response.text().catch(() => '')}`
+        );
+        process.exit(1);
+      }
+    });
+    await new Promise((resolve) => setTimeout(resolve, 200));
+  }
+};
+
+main();
